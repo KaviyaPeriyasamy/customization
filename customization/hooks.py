c4b32456b96bb266d9c0f176c6666537392b19e0
root <kaviya.3111999@gmail.com>
2021-07-08 06:31:06 +0000

    item name changes

--- customization/hooks.py
+++ customization/hooks.py
@@ -86,7 +86,9 @@ app_license = "MIT"
 # override_doctype_class = {
 # 	"ToDo": "custom_app.overrides.CustomToDo"
 # }
-
+override_doctype_class = {
+ 	"Item": "customization.item.ERPNextItem"
+}
 # Document Events
 # ---------------
 # Hook on document methods and events
